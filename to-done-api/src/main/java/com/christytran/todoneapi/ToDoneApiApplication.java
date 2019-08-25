a491115c54a59727350f68d1b0cb88af6cbb866e
Christy Tran <christytran@Christys-MacBook-Pro.local>
2019-08-25 18:20:31 -0500

    First commit of spring mvc api

---
+++ to-done-api/src/main/java/com/christytran/todoneapi/ToDoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.christytran.todoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoneApiApplication.class, args);
+	}
+
+}
---
+++ to-done-api/src/test/java/com/christytran/todoneapi/ToDoneApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.christytran.todoneapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToDoneApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
